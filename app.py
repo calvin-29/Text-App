b0b2f0321cad854225b1cfbb97a2fdbba9481efd
calvin-29 <calvin293321.com>
2025-07-03 10:18:48 +0100

    Finished version 2

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
+# import neccesary modules
+import html.entities
 from PyQt5.QtWidgets import (QApplication, QWidget, QTextEdit, QMainWindow, QMenuBar, QAction, QTabWidget, 
                              QVBoxLayout, QFileDialog, QMessageBox, QStatusBar, QLabel, QLineEdit, QHBoxLayout, 
                              QPushButton)
-from PyQt5.QtGui import QIcon, QTextCursor
-import sys, os
+from PyQt5.QtGui import QIcon, QTextCursor, QPixmap
+from PyQt5.QtCore import Qt, QDir
+import sys, os, html
 
+# text editor class
+class TextEditor(QTextEdit):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    # capture key events
+    def keyPressEvent(self, event):
+        cursor = self.textCursor()
+        current_block_text = cursor.block().text()
+
+        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            # Get indentation of current line
+            leading_whitespace = self.get_leading_whitespace(current_block_text)
+        
+            super().keyPressEvent(event)
+            if current_block_text.rstrip().endswith(":"):
+                # Insert new line with same indentation
+                cursor.insertText(leading_whitespace + "   ")
+            elif current_block_text.rstrip().endswith("{"):
+                # Insert new line with same indentation
+                cursor.insertText(leading_whitespace + "   ")
+            else:
+                # Insert new line with same indentation
+                cursor.insertText(leading_whitespace)
+        else:
+            super().keyPressEvent(event)
+
+    def get_leading_whitespace(self, text):
+        #Subtract stripped text from normal text 
+        return text[:len(text) - len(text.lstrip())]
+    
+    def setText(self, text):
+        self.textCursor().insertText(text)
+
+# main app class
 class TextApp(QMainWindow):
     fileopened = {} #index:file_path
     file_asterisk = False
@@ -12,6 +50,7 @@ class TextApp(QMainWindow):
     recent_files = set()
     def __init__(self):
         super().__init__()
+        # set window properties
         self.clip = QApplication.clipboard()
         self.setWindowTitle("Text App")
         self.setWindowIcon(QIcon(os.path.join(os.path.split(__file__)[0]+"/images","4.ico")))
@@ -34,6 +73,64 @@ class TextApp(QMainWindow):
         self.initStyle()
         self.setCentralWidget(self.window)
 
+##  method to return file type
+    @staticmethod
+    def get_file_type(name):
+        lan = ""
+        name = name.lower()
+        # put different icons for different file types
+        if name.endswith(".py"):
+            lan = "python"
+        elif name.endswith(".c"):
+            lan = "c"
+        elif name.endswith(".conf") or name.endswith(".cfg") or name.endswith(".config") or name.endswith(".ini"):
+            lan = "config"
+        elif name.endswith(".cpp"):
+            lan = "cpp"
+        elif name.endswith(".cs"):
+            lan = "csharp"
+        elif name.endswith(".go"):
+            lan = "go"
+        elif name.endswith(".html"):
+            lan = "html"
+        elif name.endswith(".jade"):
+            lan = "jade"
+        elif name.endswith(".java"):
+            lan = "java"
+        elif name.endswith(".js"):
+            lan = "javascript"
+        elif name.endswith(".json"):
+            lan = "json"
+        elif name.endswith(".less"):
+            lan = "less"
+        elif name.endswith(".markdown") or name.endswith(".md"):
+            lan = "markdown"
+        elif name.endswith(".php"):
+            lan = "php"
+        elif name.endswith(".ps1"):
+            lan = "powershell"
+        elif name.endswith(".jsx"):
+            lan = "react"
+        elif name.endswith(".rb"):
+            lan = "ruby"
+        elif name.endswith(".sass"):
+            lan = "sass"
+        elif name.endswith(".sh"):
+            lan = "shell"
+        elif name.endswith(".sql"):
+            lan = "sql"
+        elif name.endswith(".ts"):
+            lan = "typescript"
+        elif name.endswith(".vue"):
+            lan = "vue"
+        elif name.endswith(".xml"):
+            lan = "xml"
+        elif name.endswith(".yaml"):
+            lan = "yaml"
+        else:
+            lan = "default"
+        return lan
+
 #### method to add tabs
     def addNewTab(self, name, data=""):
         def changesave():
@@ -43,52 +140,72 @@ class TextApp(QMainWindow):
         widget = QWidget()
         vbox2 = QVBoxLayout(widget)
         
-        tab = QTextEdit(widget)
-
+        # initialize text editor class and change properties
+        tab = TextEditor(widget)
+        if name.endswith('.html'):
+            data = html.unescape(data)
+        print(data)
         tab.setText(data)
         tab.textChanged.connect(changesave)
         tab.setAcceptDrops(True)
-        self.tabs.addTab(widget, QIcon(os.path.join(os.path.split(__file__)[0]+"/images","pyc.png")), name)
+        lan = self.get_file_type(name)
+        icon = QPixmap(os.path.join(os.path.split(__file__)[0]+"/images", f"{lan}.ico"))
+        self.tabs.addTab(widget, QIcon(icon), name)
         self.tabs.setCurrentIndex(self.tabs.count() - 1)
         vbox2.addWidget(tab)
+        self.filesaved[self.tabs.currentIndex()] = True
         tab.cursorPositionChanged.connect(self.get_row_column)
+
+        #refresh statusbar and menubar
         self.status()
         self.menu()
 
+##  method to add and remove asterisk
     def asterisk(self):
+        ## add asterisk if file is not saved and remove it if saved
         if self.file_asterisk and not self.filesaved[self.tabs.currentIndex()]:
             index = self.tabs.currentIndex()
             if not self.tabs.tabText(index).startswith("*"):
                 if index in self.fileopened:
                     self.tabs.setTabText(index, f"*{os.path.split(self.fileopened[self.tabs.currentIndex()])[1]}")
                 else:
-                    self.tabs.setTabText(index, "*New File")
+                    self.tabs.setTabText(index, "*untitled")
         else:
             self.tabs.setTabText(self.tabs.currentIndex(), f"{os.path.split(self.fileopened[self.tabs.currentIndex()])[1]}")
             self.setWindowTitle(os.path.split(self.fileopened[self.tabs.currentIndex()])[1])
 
-#### method to get current textedit
+#### method to get text editor of the current tab 
     def current_text_edit(self):
         return self.tabs.currentWidget().findChild(QTextEdit)
 
-## double click event capture 
+## double click event capture to open a new tab
     def mouseDoubleClickEvent(self, a0):
-        self.addNewTab("New File")
+        self.addNewTab("untitled")
 
 #### method to close window tab
     def close_window(self, tab=""):
-        try:
-            self.fileopened.pop(self.tabs.currentIndex())
-        except KeyError:
-            pass
-        if tab == "":
-            self.tabs.removeTab(self.tabs.currentIndex())
+        def close():
+            try:
+                self.fileopened.pop(self.tabs.currentIndex())
+            except KeyError:
+                pass
+            if tab == "":
+                self.tabs.removeTab(self.tabs.currentIndex())
+            else:
+                self.tabs.removeTab(tab)
+            if self.tabs.count() == 0:
+                self.setWindowTitle("Text App")
+            self.menu()
+            self.status()
+
+        if self.filesaved[self.tabs.currentIndex()]:
+            close()
         else:
-            self.tabs.removeTab(tab)
-        if self.tabs.count() == 0:
-            self.setWindowTitle("Text App")
-        self.menu()
-        self.status()
+            reply = QMessageBox.warning(self, "Unsaved Tab", 
+                                "Do you want to close the unsaved tab", 
+                                QMessageBox.Yes|QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                close()
 
 #### method to create menu bar
     def menu(self):
@@ -97,15 +214,21 @@ class TextApp(QMainWindow):
 
         # File Actions
         file = menubar.addMenu("File")
-        file_commands = (["New", "Ctrl+N"], ["Save", "Ctrl+S"], ["Open", "Ctrl+O"], 
+        file_commands = (["New", "Ctrl+N"], ["Save", "Ctrl+S"], ["Open", "Ctrl+O"], ["Save As", "Ctrl+Shift+S"],
                          ["New Window", "Ctrl+Shift+N"], ["Close Tab", "Ctrl+W"], ["Quit", "Ctrl+Q"])
         for i in file_commands:
             action = QAction(i[0], self)
             action.setShortcut(i[1])
             file.addAction(action)
+
+            # Disable some menu commands if no tabs are open
             if not hasattr(self, "tabs"):
-                if action.text() == "Save" or action.text() == "Close Tab":
+                if action.text() == "Save" or action.text() == "Close Tab" or action.text() == "Save as":
                     action.setEnabled(False)
+            else:
+                if self.tabs.count() == 0:
+                    if action.text() == "Save" or action.text() == "Close Tab" or action.text() == "Save as":
+                        action.setEnabled(False)
             if i == ["Close Tab", "Ctrl+W"]:
                 file.addSeparator()
                 recent = file.addMenu("Recent Files")
@@ -142,12 +265,14 @@ class TextApp(QMainWindow):
 
 #### method to create status bar
     def status(self, row=0, column=0):
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+        # if tab is no tab currently open, the statusbar will  be empty
         if hasattr(self, 'tabs'):
-            self.statusbar = QStatusBar()
-            self.setStatusBar(self.statusbar)
-            self.status_label = QLabel()
-            self.statusbar.addPermanentWidget(self.status_label, 0)
-            self.status_label.setText(f"Ln {row}, Col {column}")
+            if self.tabs.count() > 0:
+                self.status_label = QLabel()
+                self.statusbar.addPermanentWidget(self.status_label, 0)
+                self.status_label.setText(f"Ln {row}, Col {column}")
 
 #### method to display row and column 
     def get_row_column(self):
@@ -179,11 +304,13 @@ class TextApp(QMainWindow):
         command = command.text().lower()
         match command:
             case "new":
-                self.addNewTab("New File")
+                self.addNewTab("untitled")
             case "save":
                 self.file("s")
             case "open":
                 self.file("o")
+            case "save as":
+                self.file("s", ["saveas", ''])
             case "new window":
                 new_win = TextApp()
                 new_win.setGeometry(self.x()-10, self.y()+50, self.width(), self.height())
@@ -224,7 +351,6 @@ class TextApp(QMainWindow):
     def add_recent(self):
         try:
             path = os.path.split(__file__)[0]+"/recent.lst"
-            # print(self.recent_files)
             with open(path, "r") as k:
                 for j in k.readlines():
                     self.recent_files.add(f"{j}")
@@ -237,9 +363,36 @@ class TextApp(QMainWindow):
 
 #### method to open and save file
     def file(self, file_handling, mode=[0,""]):
+        files = """
+            All Files(* *.);;
+            Text Files (*.txt);;
+            Python Files (*.py *.pyw *.pyc);;
+            C Files (*.c);;
+            Config Files (*.conf *.cfg *.config *.ini);;
+            C++ Files (*.cpp);;
+            C# Files (*.cs);;
+            CSS Files (*.css);;
+            GO Files (*.go);;
+            HTML Files (*.html);;
+            Jade Files (*.jade);;
+            Java Files (*.java);;
+            JSON Files (*.json);;
+            LESS Files (*.less);;
+            Markdown Files (*.md *.markdown);;
+            PHP Files (*.php);;
+            Powershell Files (*.ps1);;
+            React Files (*.jsx);;
+            Ruby Files (*.rb);;
+            Sass Files (*.sass);;
+            Shell Files (*.sh);;
+            SQL Files (*.sql);;
+            TypeScript Files (*.ts);;
+            Vue Files (*.vue);;
+            XML Files (*.xml);;
+            Yaml Files (*.yaml);;
+        """
         if file_handling == "o":
             if mode[0] == 1:
-                print(mode[1])
                 if os.path.exists(mode[1]):
                     with open(mode[1], "r") as f:
                         # get filename name
@@ -256,9 +409,14 @@ class TextApp(QMainWindow):
 
             else:
                 while True:
-                    file = QFileDialog.getOpenFileName(self, "Open File", os.path.split(__file__)[0], 
-                                                    "All Files(* *.);;Text Files (*.txt);;Python Files (*.py *.pyw *.pyc)"
-                                                    ,"Python Files (*.py *.pyw *.pyc)")
+                    current = ""
+                    try:current = QDir.homePath() if self.tabs.count() == 0 else os.path.split(self.fileopened[self.tabs.currentIndex()])[0]
+                    except:pass
+                    dialog = QFileDialog()
+                    dialog.setMinimumSize(300, 200)
+                    file = dialog.getOpenFileName(self, "Open File", current, 
+                                                    files
+                                                    ,"Python Files (*.py *.pyw *.pyc)",)
                     try:
                         if file[0] not in self.fileopened.values():
                             with open(file[0], "r") as f:
@@ -268,8 +426,6 @@ class TextApp(QMainWindow):
                                 # open a new tab and select that tab
                                 self.addNewTab(filename, f.read())
                                 self.tabs.setCurrentIndex(self.tabs.count()-1)
-                                self.fileopened[self.tabs.currentIndex()] = file[0]
-                                self.filesaved[self.tabs.currentIndex()] = True
                                 self.recent_files.add(f"{file[0]}\n")
                                 self.add_recent()
                         else:
@@ -284,9 +440,11 @@ class TextApp(QMainWindow):
                         QMessageBox(QMessageBox.Critical, "Unknown file type", "File type is unknown").exec_()
                         continue
         elif file_handling == "s":
-            if self.tabs.count() > 0 and self.tabs.currentIndex() not in self.fileopened:
-                file = QFileDialog.getSaveFileName(self, "Save File", os.path.split(__file__)[0], 
-                                                "All Files(* *.);;Text Files (*.txt);;Python Files (*.py *.pyw *.pyc)"
+            if self.tabs.count() > 0 and self.tabs.currentIndex() not in self.fileopened or mode[0] == "saveas":
+                dialog = QFileDialog()
+                dialog.setMinimumSize(300, 200)
+                file = dialog.getSaveFileName(self, "Save File", os.path.split(__file__)[0], 
+                                                files
                                                 ,"Python Files (*.py *.pyw *.pyc)")
                 try:
                     with open(file[0], "w") as f:
@@ -296,6 +454,11 @@ class TextApp(QMainWindow):
                         # create a file and write the content of the tab in it change the tab's text
                         f.write(self.current_text_edit().toPlainText())
                         self.tabs.setTabText(self.tabs.currentIndex(), self.filename)
+                        lan = self.get_file_type(self.filename)
+                        icon = QPixmap(os.path.join(os.path.split(__file__)[0]+"/images", f"{lan}.ico"))
+                        self.tabs.setTabIcon(self.tabs.currentIndex(), 
+                                             QIcon(icon)
+                                            )
                         self.setWindowTitle(f"{self.filename}")
                         self.fileopened[self.tabs.currentIndex()] = file[0]
                         self.filesaved[self.tabs.currentIndex()] = True
